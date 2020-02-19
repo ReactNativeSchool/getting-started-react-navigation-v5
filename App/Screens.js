d1c2c27f74ca6ff8febe773b53930bde9e070811
Spencer Carli <spencercarli@gmail.com>
2020-02-19 10:45:04 -0600

    WRiting the codes

--- App/Screens.js
+++ App/Screens.js
@@ -21,26 +21,45 @@ const ScreenContainer = ({ children }) => (
   <View style={styles.container}>{children}</View>
 );
 
-export const Home = () => (
+export const Home = ({ navigation }) => (
   <ScreenContainer>
     <Text>Master List Screen</Text>
-    <Button title="React Native by Example" onPress={() => alert("todo!")} />
-    <Button title="React Native School" onPress={() => alert("todo!")} />
-    <Button title="Drawer" onPress={() => alert("todo!")} />
+    <Button
+      title="React Native by Example"
+      onPress={() =>
+        navigation.push("Details", { name: "React Native by Example " })
+      }
+    />
+    <Button
+      title="React Native School"
+      onPress={() =>
+        navigation.push("Details", { name: "React Native School" })
+      }
+    />
+    <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
   </ScreenContainer>
 );
 
-export const Details = () => (
+export const Details = ({ route }) => (
   <ScreenContainer>
     <Text>Details Screen</Text>
+    {route.params.name && <Text>{route.params.name}</Text>}
   </ScreenContainer>
 );
 
 export const Search = ({ navigation }) => (
   <ScreenContainer>
     <Text>Search Screen</Text>
-    <Button title="Search 2" onPress={() => alert("todo!")} />
-    <Button title="React Native School" onPress={() => alert("todo!")} />
+    <Button title="Search 2" onPress={() => navigation.push("Search2")} />
+    <Button
+      title="React Native School"
+      onPress={() => {
+        navigation.navigate("Home", {
+          screen: "Details",
+          params: { name: "React Native School" }
+        });
+      }}
+    />
   </ScreenContainer>
 );
 
@@ -51,11 +70,13 @@ export const Search2 = () => (
 );
 
 export const Profile = ({ navigation }) => {
+  const { signOut } = React.useContext(AuthContext);
+
   return (
     <ScreenContainer>
       <Text>Profile Screen</Text>
-      <Button title="Drawer" onPress={() => alert("todo!")} />
-      <Button title="Sign Out" onPress={() => alert("todo!")} />
+      <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
+      <Button title="Sign Out" onPress={() => signOut()} />
     </ScreenContainer>
   );
 };
@@ -67,20 +88,27 @@ export const Splash = () => (
 );
 
 export const SignIn = ({ navigation }) => {
+  const { signIn } = React.useContext(AuthContext);
+
   return (
     <ScreenContainer>
       <Text>Sign In Screen</Text>
-      <Button title="Sign In" onPress={() => alert("todo!")} />
-      <Button title="Create Account" onPress={() => alert("todo!")} />
+      <Button title="Sign In" onPress={() => signIn()} />
+      <Button
+        title="Create Account"
+        onPress={() => navigation.push("CreateAccount")}
+      />
     </ScreenContainer>
   );
 };
 
 export const CreateAccount = () => {
+  const { signUp } = React.useContext(AuthContext);
+
   return (
     <ScreenContainer>
       <Text>Create Account Screen</Text>
-      <Button title="Sign Up" onPress={() => alert("todo!")} />
+      <Button title="Sign Up" onPress={() => signUp()} />
     </ScreenContainer>
   );
 };
--- App/index.js
+++ App/index.js
@@ -1,19 +1,142 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { AuthContext } from "./context";
+import {
+  SignIn,
+  CreateAccount,
+  Search,
+  Home,
+  Details,
+  Search2,
+  Profile,
+  Splash
+} from "./Screens";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen
+      name="SignIn"
+      component={SignIn}
+      options={{ title: "Sign In" }}
+    />
+    <AuthStack.Screen
+      name="CreateAccount"
+      component={CreateAccount}
+      options={{ title: "Create Account" }}
+    />
+  </AuthStack.Navigator>
+);
+
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={Home} />
+    <HomeStack.Screen
+      name="Details"
+      component={Details}
+      options={({ route }) => ({
+        title: route.params.name
+      })}
+    />
+  </HomeStack.Navigator>
+);
+
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Search2" component={Search2} />
+  </SearchStack.Navigator>
+);
+
+const ProfileStack = createStackNavigator();
+const ProfileStackScreen = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name="Profile" component={Profile} />
+  </ProfileStack.Navigator>
+);
+
+const TabsScreen = () => (
+  <Tabs.Navigator>
+    <Tabs.Screen name="Home" component={HomeStackScreen} />
+    <Tabs.Screen name="Search" component={SearchStackScreen} />
+  </Tabs.Navigator>
+);
+
+const Drawer = createDrawerNavigator();
+const DrawerScreen = () => (
+  <Drawer.Navigator initialRouteName="Profile">
+    <Drawer.Screen name="Home" component={TabsScreen} />
+    <Drawer.Screen name="Profile" component={ProfileStackScreen} />
+  </Drawer.Navigator>
+);
+
+const RootStack = createStackNavigator();
+const RootStackScreen = ({ userToken }) => (
+  <RootStack.Navigator headerMode="none">
+    {userToken ? (
+      <RootStack.Screen
+        name="App"
+        component={DrawerScreen}
+        options={{
+          animationEnabled: false
+        }}
+      />
+    ) : (
+      <RootStack.Screen
+        name="Auth"
+        component={AuthStackScreen}
+        options={{
+          animationEnabled: false
+        }}
+      />
+    )}
+  </RootStack.Navigator>
+);
+
+export default () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [userToken, setUserToken] = React.useState(null);
+
+  const authContext = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setIsLoading(false);
+        setUserToken("asdf");
+      },
+      signUp: () => {
+        setIsLoading(false);
+        setUserToken("asdf");
+      },
+      signOut: () => {
+        setIsLoading(false);
+        setUserToken(null);
+      }
+    };
+  }, []);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  if (isLoading) {
+    return <Splash />;
   }
-});
+
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <RootStackScreen userToken={userToken} />
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+};
